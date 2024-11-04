420290a30a14fdf0c1b1c97c6d28655d85dd0f06
UkrAndy <brila_andrij@ukr.net>
2024-11-04 19:19:49 +0200

    init

---
+++ models/user/UsersDBService.mjs
@@ -0,0 +1,34 @@
+import User from './User.mjs'
+import mongoose from 'mongoose'
+
+class UsersDBService {
+  static async getList(filters) {
+    try {
+      return await User.find(filters, { password: 0 })
+    } catch (error) {
+      return []
+    }
+  }
+
+  static async create(data) {
+    const user = new User(data)
+    return await user.save()
+  }
+
+  static async getById(id) {
+    return await User.findById(id)
+  }
+
+  static async update(id, data) {
+    return await User.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    })
+  }
+
+  static async deleteById(id) {
+    return await User.findByIdAndDelete(id)
+  }
+}
+
+export default UsersDBService
